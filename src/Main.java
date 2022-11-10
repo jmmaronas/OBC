47e8216b2a0059d011c9e65f67b6043536ff585a
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-11-10 19:52:55 -0300

    creado funcion suma, class Coche con metodo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(suma(2, 4, 6));
+        Coche myCoche = new Coche();
+        myCoche.incremento();
+        System.out.println(myCoche.numeroPuertas);
+    }
+
+    public static int suma(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
     }
 }
+
+    class Coche {
+        int numeroPuertas = 4;
+
+        public void incremento() {
+            this.numeroPuertas ++;
+        }
+
+
+    }
